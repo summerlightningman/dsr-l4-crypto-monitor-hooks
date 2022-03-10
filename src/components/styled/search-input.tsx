10dbc68f51229c48c5ebde69fec9e8d5972a4672
summerlightningman <div8@bk.ru>
2022-03-10 23:00:02 +0300

    added styles for search-input

--- src/components/styled/search-input.tsx
+++ src/components/styled/search-input.tsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 
 const SearchInput = styled.input`
-  
+  width: 60vw;
+  height: 50px;
+  outline: none;
+  box-shadow: 0 5px 20px pink;
+  border-radius: 10px;
+  font-size: 16pt;
+  padding: 0 10px;
 `;
 
 export default SearchInput;
