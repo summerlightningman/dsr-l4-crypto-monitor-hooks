b959882b90712c9d62c1d7d2c6cae6097e3bc5a6
summerlightningman <div8@bk.ru>
2022-04-18 00:54:12 +0300

    React updated to 18 version

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import App from './components/app';
 import GlobalStyles from './global-styles';
 
+const container = document.getElementById('root')!;
+const root = ReactDOMClient.createRoot(container);
 
-ReactDOM.render(
+root.render(
     <React.StrictMode>
         <GlobalStyles/>
         <App/>
-    </React.StrictMode>,
-    document.getElementById('root')
+    </React.StrictMode>
 );
